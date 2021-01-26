017f0e52ee894d4a5bc6f8d7382d17973ca5cf45
Sara Wilcox <saraewilcox@gmail.com>
2021-01-26 10:26:44 +0000

    deploy to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require( 'express' );
 const app = express();
 
 app.get('/', function (req, res) {
-  res.render('index', {});
+  res.render('./routes/index.js', {});
 });
 
 // listen on a port
