d1151e140d56bca794e0ca6ca8ff4ba038f45f1a
Sara Wilcox <saraewilcox@gmail.com>
2021-01-26 12:56:21 +0000

    deploy to heroku

--- app.js
+++ app.js
@@ -98,20 +98,6 @@ app.use('/', streetart);
 const api = require('./routes/api');
 app.use('/', api);
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 8080);
-  res.render('./error/error');
-});
-
 module.exports = app;
 
 
